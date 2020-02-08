40964b9441a96fd212545a0b4e86cbc394ef0428
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-08 14:40:27 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,22 +1,19 @@
 # Examples inspired by U. S. National Organic Standards
 
-ORGANIC_PRODUCE = [
-  "Strawberries",
-  "Potatoes",
-  "Grapes",
-  "Avocadoes",
-  "Asparagus"
-]
+ORGANIC_PRODUCE = ["Strawberries","Potatoes","Grapes","Avocadoes","Asparagus"]
 
-CONVENTIONAL_PRODUCE = [
-  "Grapefruit",
-  "Pineapple",
-  "Oranges",
-  "Watermelon",
-  "Eggplant"
-]
+CONVENTIONAL_PRODUCE = ["Grapefruit","Pineapple","Oranges","Watermelon","Eggplant"]
 
 def assembled_matrix
+
+produce_storage_room = [
+
+CONVENTIONAL_PRODUCE,
+ORGANIC_PRODUCE
+
+]
+
+produce_storage_room
   # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
@@ -24,18 +21,30 @@ def assembled_matrix
 end
 
 def sorted_matrix
-  # Using Array literal syntax only, build another nested array that 
+produce_storage_room = [
+
+  CONVENTIONAL_PRODUCE.sort,
+  ORGANIC_PRODUCE.sort
+
+]
+  # Using Array literal syntax only, build another nested array that
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
 end
 
 def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
+
+
+matrix[row][column]
+  # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
 end
 
 def matrix_update(matrix, row, column, new_value)
-  # Given any matrix (array of arrays), a row index and a column index, 
+
+matrix[row][column] = new_value
+  # Given any matrix (array of arrays), a row index and a column index,
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
+  return matrix
 end
